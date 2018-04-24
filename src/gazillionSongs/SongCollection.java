d39d7d7c59c849c67540341c22da1db187bc9b08
Jon Fincher <jfincher42@gmail.com>
2018-04-24 10:04:34 -0700

    Merge sort working

--- src/gazillionSongs/SongCollection.java
+++ src/gazillionSongs/SongCollection.java
@@ -63,7 +63,7 @@ public class SongCollection {
 			Song value = songList.get(j); // value is the next element to insert
 			int possibleIndex = j;
 			while (possibleIndex > 0 && value.compareTo(songList.get(possibleIndex - 1), field) < 0) {
-				songList.set(possibleIndex, songList.get(possibleIndex-1));
+				songList.set(possibleIndex, songList.get(possibleIndex - 1));
 				possibleIndex--;
 			}
 			songList.set(possibleIndex, value); // put value in its place!
@@ -72,8 +72,52 @@ public class SongCollection {
 	}
 
 	public boolean mergeSort(String field) {
+		int n = songList.size();
+		ArrayList<Song> temp = new ArrayList<Song>(songList);
+		mergeSortX(songList, 0, n - 1, temp, field);
 
 		return true;
+	}
+
+	private void mergeSortX(ArrayList<Song> orig, int from, int to, ArrayList<Song> temp, String field) {
+		// If from >= to, then there's nothing to sort
+		if (from < to) {
+			// Calculate the middle element, then sort each half
+			int middle = (from + to) / 2;
+			mergeSortX(orig, from, middle, temp, field);
+			mergeSortX(orig, middle + 1, to, temp, field);
+			// When done, merge the two sorted halves together
+			merge(orig, from, middle, to, temp, field);
+		}
+	}
+
+	private void merge(ArrayList<Song> orig, int from, int middle, int to, ArrayList<Song> temp, String field) {
+		int nextLeft = from; // The next element of the left section
+		int nextRight = middle + 1; // The next element of the right section
+		int nextMerge = from; // Where to put the next lowest element in temp
+
+		// If there are elements in both sections
+		while (nextLeft <= middle && nextRight <= to) {
+			// Find the lowest element, and copy it to temp
+			// We can simplify this a bit
+			if (orig.get(nextLeft).compareTo(orig.get(nextRight), field) <= 0)
+				temp.set(nextMerge, orig.get(nextLeft++));
+			else
+				temp.set(nextMerge, orig.get(nextRight++));
+			nextMerge++;
+		}
+
+		// Does the left section have any elements remaining? Copy them over
+		while (nextLeft <= middle)
+			temp.set(nextMerge++, orig.get(nextLeft++));
+
+		// Does the right section have anay elements remaining? Copy them over
+		while (nextRight <= to)
+			temp.set(nextMerge++, orig.get(nextRight++));
+
+		// Now blast the whole thing back into the source array
+		for (int copyMerge = from; copyMerge <= to; copyMerge++)
+			orig.set(copyMerge, temp.get(copyMerge));
 	}
 
 	public boolean quickSort(String field) {
